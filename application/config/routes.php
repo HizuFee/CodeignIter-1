890b288c4c618f8d1a6901595d8e65919685b2b6
Sandy Triananda <legangamerz@gmail.com>
2025-02-25 08:04:59 +0700

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -86,7 +86,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = "Mahasiswa";
+$route['default_controller'] = "mahasiswa";
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
